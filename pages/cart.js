2466c3f454291eec0129b40f375dc26f007434dc
djoda369 <djordje.stankovic.priv@gmail.com>
2023-06-29 15:25:26 +0200

    Pokusaj ponovo

--- pages/cart.js
+++ pages/cart.js
@@ -18,12 +18,11 @@ export default function Cart() {
     let cartItems = JSON.parse(localStorage.getItem("cartNitNis"));
     if (cartItems === null && cartItems === undefined) {
       cartItems = localStorage.setItem("cartNitNis");
-      if (cartItems !== null && cartItems !== undefined) {
-        const price = cartItems
-          .map((product) => product.price)
-          .reduce((partialSum, a) => partialSum + a, 0);
-        setPrice(price);
-      }
+    } else if (cartItems !== null && cartItems !== undefined) {
+      const price = cartItems
+        .map((product) => product.price)
+        .reduce((partialSum, a) => partialSum + a, 0);
+      setPrice(price);
     }
     setIsLoading(false);
     setItems(cartItems);
