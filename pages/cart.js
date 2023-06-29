29c5fdd30e843e86afb88402a78e85b811b6391b
djoda369 <djordje.stankovic.priv@gmail.com>
2023-06-29 15:21:05 +0200

    Opet proba

--- pages/cart.js
+++ pages/cart.js
@@ -17,13 +17,14 @@ export default function Cart() {
   useEffect(() => {
     let cartItems = JSON.parse(localStorage.getItem("cartNitNis"));
     if (cartItems === null && cartItems === undefined) {
-      localStorage.setItem("cartNitNis");
-      cartItems = JSON.parse(localStorage.getItem("cartNitNis"));
+      cartItems = localStorage.setItem("cartNitNis");
+      if (cartItems !== null && cartItems !== undefined) {
+        const price = cartItems
+          .map((product) => product.price)
+          .reduce((partialSum, a) => partialSum + a, 0);
+        setPrice(price);
+      }
     }
-    const price = cartItems
-      .map((product) => product.price)
-      .reduce((partialSum, a) => partialSum + a, 0);
-    setPrice(price);
     setIsLoading(false);
     setItems(cartItems);
   }, []);
