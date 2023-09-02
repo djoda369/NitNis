7b3eb9618e6be8476e856c31f94a696a29967d1a
djoda369 <djordje.stankovic.priv@gmail.com>
2023-09-02 14:51:12 +0200

    Prepravka prepravke

--- pages/prodavnica/[shop].js
+++ pages/prodavnica/[shop].js
@@ -189,6 +189,6 @@ export async function getStaticPaths() {
 
   return {
     paths: path,
-    fallback: true,
+    fallback: false,
   };
 }
--- pages/prodavnica/[shop]/[product].js
+++ pages/prodavnica/[shop]/[product].js
@@ -687,6 +687,6 @@ export async function getStaticPaths() {
 
   return {
     paths: path,
-    fallback: false,
+    fallback: true,
   };
 }
